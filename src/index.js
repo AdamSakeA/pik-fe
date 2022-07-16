1cc98faec1052bc44aa69fc073e321053480e90e
AdamSakeA <adamsake8@gmail.com>
2022-07-16 20:21:04 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
